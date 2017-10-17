4c485d97f9b871ca60606b5fadb3ada11d875457
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-10-17 01:41:19 +0100

    view: add section action to component

--- resources/views/components/dashboard/section.blade.php
+++ resources/views/components/dashboard/section.blade.php
@@ -1,7 +1,12 @@
 <section class="{{ !empty($padding) ? 'section-padding' : '' }}">
     <div class="container-fluid">
         <header> 
-            <h1 class="h3 display">{{ $title }}</h1>
+            <h1 class="h3 d-inline-block">{{ $title }}</h1>
+            <div class="section-actions">
+                @if(!empty($actions))
+                    {{ $actions }}
+                @endif
+            </div>
         </header>
 
         {{ $slot }}
